57d18661ebe0fb477ee353434dbf07051642093d
luke <onlydj96@naver.com>
2024-07-08 11:32:41 +0900

    dataset 및 sample image data 추가

--- test_image.sh
+++ test_image.sh
@@ -1,9 +1,2 @@
-
-# Original
-# python3 test_nwpu.py --model clip-resnet50 --truncation 4 --weight_path weights/best_mae_0.pth --device cuda:0
-
 # Torch custom
 python3 test_torch.py --model clip-resnet50 --truncation 4 --weight_path weights/best_mae_0.pth --device cuda:0
-
-# ONNX 
-# python3 test_onnx.py --weight_path weights/clip_ebc.onnx --device cuda:0
